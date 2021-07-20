279eed8816fe676dd2469dbd3195afbaa16d5775
zxy <zhaoxuyang@edenep.net>
2021-07-20 14:34:05 +0800

    first

---
+++ app/src/main/java/com/zyx_hunan/wanmvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zyx_hunan.wanmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
